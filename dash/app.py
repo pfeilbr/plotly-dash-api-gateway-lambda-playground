629af21ca22fc1904aa5f17bd8ae478f3368bfc7
Brian Pfeil <brian.pfeil@gmail.com>
2022-04-04 14:09:43 -0400

    init

---
+++ dash/app.py
@@ -0,0 +1,32 @@
+import awsgi
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, compress=False, requests_pathname_prefix='/Prod/')
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+def lambda_handler(event, context):
+    return awsgi.response(app.server, event, context)
